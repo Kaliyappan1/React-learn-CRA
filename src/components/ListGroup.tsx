3ff664611c386691d513e016607e9039f7058311
kaliyappan1 <yappank17@gmail.com>
2024-05-09 06:10:55 +0530

    list item and no item found is dynamic and add event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,18 @@ function ListGroup() {
   let items = [
     "red", "green", "blue", "yello ", "orange"
   ];
-  // items = [];
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p> }
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+           className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}>{item}
+          </li>
         ))}
       </ul>
     </>
