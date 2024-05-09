ebf6c2fc7f66ee1d2b6701a6e01dc1d9d2ba16f5
kaliyappan1 <yappank17@gmail.com>
2024-05-09 07:04:39 +0530

    dynamic click function to activate list item manage state using hook - use state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
+
 function ListGroup() {
-  let items = [
-    "red", "green", "blue", "yello ", "orange"
-  ];
+  const items = ["red", "green", "blue", "red", "green", "blue"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
-    <>
+    <ul className="list-group">
       <h1>List</h1>
-      {items.length === 0 && <p>No item found</p> }
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-           className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(event)}>{item}
-          </li>
-        ))}
-      </ul>
-    </>
+      {items.length === 0 && <p>no items found</p>}
+      {items.map((item, index) => (
+        <li
+          className={
+            selectedIndex === index
+              ? "list-group-item active"
+              : "list-group-item"
+          }
+          onClick={() => {setSelectedIndex(index)}}
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
   );
 }
 
